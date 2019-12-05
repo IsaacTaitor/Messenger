a2fdfa76792b4f830a28c27ab07f888963fc6efb
Andrey Malinin <isaac220515@gmail.com>
2019-12-05 22:21:04 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "@typescript-eslint/eslint-plugin"
+    ],
+    "rules": {
+        "indent": [
+            "warn",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-debugger": "off",
+        /*"linebreak-style": ["error", "unix"]*/
+        /*"linebreak-style": ["error", "windows"],*/
+        "linebreak-style": 0,
+        //"@typescript-eslint/explicit-function-return-type": "off",
+        //"@typescript-eslint/ban-ts-ignore": "off"
+    }
+};
